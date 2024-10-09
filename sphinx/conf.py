f15fbae044a83a98cfe69b1c35445640ba39b5e0
Maxim Reznik <reznikmm@gmail.com>
2024-10-09 20:22:45 +0300

    Add preface.md

---
+++ sphinx/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GNAT book'
+copyright = '2004, Javier Miranda and Edmond Schonberg'
+author = 'Javier Miranda, Edmond Schonberg'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'uk'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
+html_static_path = ['_static']
